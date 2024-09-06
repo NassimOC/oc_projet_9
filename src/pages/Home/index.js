cf494a67cbb718428cb0f0cb88196d717177192e
Nassim <nassim-sadik@hotmail.com>
2024-09-06 21:02:34 +0200

    Fix several warnings

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -116,13 +116,14 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
+          {last? 
           <EventCard
           imageSrc={last?.cover}
           title={last?.title}
           date={new Date(last?.date)}
           small
           label="boom"
-          />
+          /> : null}
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
